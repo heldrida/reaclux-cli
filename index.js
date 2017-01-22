7d98477a68ea2e15b42593f00819afdd54b4dbae
Helder <heldrida@gmail.com>
2017-01-22 18:01:17 +0000

    amend the banner output

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 #!/usr/bin/env node
-console.log('Reaclux: Hello, world!');
+
+require('babel-polyfill')
+
+var spawn = require('child_process').spawn,
+	chalk = require('chalk'),
+	figlet = require('figlet')
+
+spawn('clear', [null], { stdio: 'inherit' })
+
+console.log(
+	chalk.magenta(
+		figlet.textSync('Reaclux', { horizontalLayout: 'full' })
+	),
+	chalk.yellow.bold('\n' + ' ' + 'CLI'),
+	chalk.yellow('by Punkbit'),
+	'\n',
+	'\n'
+)
